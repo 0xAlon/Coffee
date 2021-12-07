93b9f7659a75c0b74933c296326ecdafd72fd854
Alon <alon.ttp@gmail.com>
2021-12-07 10:33:04 +0200

    Login with firebase
    Create sidebar menu and add login activity with firebase authentication.

--- app/src/main/java/com/networks/coffee/MainActivity.java
+++ app/src/main/java/com/networks/coffee/MainActivity.java
@@ -1,14 +1,28 @@
 package com.networks.coffee;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //super.onCreate(savedInstanceState);
+
+        View rootView = getLayoutInflater().inflate(R.layout.activity_main, frameLayout);
+
+        View clickView = rootView.findViewById(R.id.nav);
+        clickView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
